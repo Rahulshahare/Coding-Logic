515eec748b32cf3f17688d075e3a24df66a5c4f0
rahulshahare <rahulshahare@gmail.com>
2017-02-05 18:22:50 +0530

    Hello World
    How to say hello world in php is define in this file. along with that
    file contains some basic info about php.

---
+++ PHP/1.HelloWorld.php
@@ -0,0 +1,23 @@
+<?php
+/*   Author : Rahul Shahare 
+            Web Developer. 
+            (https://github.com/Rahulshahare/Coding-Logic) 
+*/
+
+/* Php is a basically Progamming language (Scripting Language) Specially built for Web Development. 
+   and Php can be embedded in HTML easily.
+   today whatever i learn about php , i learn it by my self. 
+   so lets say HELLO to the WORLD in php.
+*/
+
+echo"HELLO WORLD";
+
+//Here we have said helloWorld in php, its a PHP echo function that prints. 
+/*
+    Few Line about Php :
+                    Php is a high level web programming language. and php have interpreter to conver this high level
+                    language to low (Machine) level language. 
+                    Searh interpreter or compiler to know more about it and how it works. 
+*/
+
+?>
